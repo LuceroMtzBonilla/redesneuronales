3abd9ca9c94429932904701c5620e89314640678
LuceroMtzBonilla <114697211+LuceroMtzBonilla@users.noreply.github.com>
2022-09-29 17:59:54 -0500

    implementacion cross entropy y SGD con momentum
    se agrego cross entropy a la red neuronal, lo que significa que se cambio la funcion de costo y con esto el gradiente por lo que los errores son diferentes y los parametros w y b actualizan de otra manera. Esta red mostro buen resultado obteniendo una eficiencia de hasta 93%.

---
+++ network-SGD-momentum-cross-entropy.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+#### Libraries
+# Standard library
+import random
+
+# Third-party libraries
+import numpy as np
+class Network(object):
+  #_init_ es el constructor  
+    def __init__(self, sizes):
+        """The list ``sizes`` contains the number of neurons in the
+        respective layers of the network.  For example, if the list
+        was [2, 3, 1] then it would be a three-layer network, with the
+        first layer containing 2 neurons, the second layer 3 neurons,
+        and the third layer 1 neuron.  The biases and weights for the
+        network are initialized randomly, using a Gaussian
+        distribution with mean 0, and variance 1.  Note that the first
+        layer is assumed to be an input layer, and by convention we
+        won't set any biases for those neurons, since biases are only
+        ever used in computing the outputs from later layers."""
+        
+        #definiendo  las variables internas
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self, a): 
+        """Return the output of the network if ``a`` is input."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+            
+        return a
+
+    #Algoritmo del optimizador Stochastic Gradient Descent 
+    def SGD(self, training_data, epochs, mini_batch_size, eta,
+            test_data=None):
+        """Train the neural network using mini-batch stochastic
+        gradient descent.  The ``training_data`` is a list of tuples
+        ``(x, y)`` representing the training inputs and the desired
+        outputs.  The other non-optional parameters are
+        self-explanatory.  If ``test_data`` is provided then the
+        network will be evaluated against the test data after each
+        epoch, and partial progress printed out.  This is useful for
+        tracking progress, but slows things down substantially."""
+
+        training_data = list(training_data)
+        n = len(training_data)
+
+        if test_data:
+            test_data = list(test_data)
+            n_test = len(test_data)
+
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch_SGD(mini_batch, eta)
+            if test_data:
+                print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test))
+            else:
+                print("Epoch {} complete".format(j))
+
+    #Algoritmo para actualizar los pesos de la red neuronal
+    #Aprendizaje y optimizacion de la red 
+    def update_mini_batch_SGD(self, mini_batch, eta):
+        """Update the network's weights and biases by applying
+        gradient descent using backpropagation to a single mini batch.
+        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
+        is the learning rate."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
+    
+   #Algoritmo del optimizador Stochastic Gradient Descent con momentum     
+    def SGD_momentum(self, training_data, epochs, mini_batch_size, eta, momentum,
+            test_data=None):
+
+        training_data = list(training_data)
+        n = len(training_data)
+
+        if test_data:
+            test_data = list(test_data)
+            n_test = len(test_data)
+
+        for j in range(epochs): #haremos la segmentacion de acuerdo al numero de epocas que tengamos. 
+        
+        #si tenemos 30 epocas significa que segmentaremos todos nuestros datos de entr. de forma aleatoria 30 veces.
+            random.shuffle(training_data)
+            
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in range(0, n, mini_batch_size)] #segmentacion de los datos de entrenamiento, los volvemos 
+                #mini_batches. 
+
+            for mini_batch in mini_batches:
+                self.update_mini_batch_SGD_momentum(mini_batch, eta, momentum)
+            if test_data:
+                print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test)) #devuelve 
+                #por ejemplo:  Epoch 9 : 9501 / 10000. numero de epoca,     y el numero total de datos de prueba
+            else:
+                print("Epoch {} complete".format(j))
+    
+    
+    def update_mini_batch_SGD_momentum(self, mini_batch, eta, momentum ):
+        #agregamos un vector de ceros para vdw y vdb, los cuales almacenaran informacion sobre el momentum
+        nabla_b = [np.zeros(b.shape) for b in self.biases] #vector de ceros con la forma de b
+        nabla_w = [np.zeros(w.shape) for w in self.weights] #vector de ceros con la forma de w
+        vdw=[np.zeros(w.shape) for w in self.weights] 
+        vdb=[np.zeros(b.shape) for b in self.biases]
+        
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            vdw = [momentum*nvdw + (1-momentum)*(dnw) for nvdw, dnw in zip(vdw, nabla_w)]
+            vdb = [momentum*nvdb +(1-momentum)*(dnb) for nvdb,dnb in zip(vdb, nabla_b)]
+        
+        
+        
+        self.weights = [w-eta*nvdw for w, nvdw in zip(self.weights, vdw)]
+        self.biases = [b-eta*nvdb for b, nvdb in zip(self.biases, vdb)]
+    
+    
+    
+    
+    
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+    # feedforward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            z = np.dot(w, activation)+b #z=wx+b
+            zs.append(z)  #agregamos cada z a la lista de zs
+            activation = sigmoid(z)
+            activations.append(activation)
+    # backward pass
+    #al utilizar cross entropy cambiamos la funcion de costo y dado que los pesos
+    #se actualizan de acuerdo a la derivada de la funcion de costo, estos valores 
+    #cambian, por lo que el error delta es diferente y las nablas tambien
+        delta = 1/len(activations)*self.cost_derivative(activations[-1], y) * \
+        sigmoid_prime(zs[-1]) 
+        nabla_b[-1] = delta   
+    #respecto a b
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose()) 
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+            return (nabla_b, nabla_w)
+
+def evaluate(self, test_data):
+    test_results = [(np.argmax(self.feedforward(x)), y)
+                    for (x, y) in test_data]
+    return sum(int(x == y) for (x, y) in test_results)
+
+#usamos la funcion de costo de cross entropy
+def cost_derivative(self, output_activations, y):
+    return (output_activations-y)/output_activations*(1-output_activations)
+
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
+
